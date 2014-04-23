2cab6b3ffe196a5d3251fe40a00fd476de4bc4a8
WAGAZ <william.a.garris@gmail.com>
2014-04-23 08:14:10 -0700

    William Garris R Programming Week 2 Assignment
    Created two functions that will provide the inverse of a matrix and
    then cache the solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##  The two functions below can be used to solve and/or cache the solution for 
+##  providing the inverse of a matrix.  that are used to create a special object that stores a numeric vector and caches its mean.
+ 
+
+
+## This function uses the << symbol to store the solution for the matrix inverse within a different 
+## environment (function) for future reference.  If called upon by the cacheSolve function and the 
+## matrix inverse solution is available, it is provided, thus saving time.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inv <<- solve
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
+## The following function calculates the solution for solving the inverse of a matrix.  What it
+## does first is to see if the solution has been already completed and cached in the above function.
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
+
+> g <- makeCacheMatrix(matrix1)
+> cacheSolve(g)
+[,1] [,2] [,3]
+[1,]    7   -3   -3
+[2,]   -1    1    0
+[3,]   -1    0    1
+> cacheSolve(g)
+getting cached data
+[,1] [,2] [,3]
+[1,]    7   -3   -3
+[2,]   -1    1    0
+[3,]   -1    0    1
+> 
